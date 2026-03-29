8220833bd4e3f0dd13774f10d457d141a43ba7d0
Eddie <eddieskeete@gmail.com>
2026-03-29 02:39:26 -0400

    learning

--- Learning/hello.py
+++ Learning/hello.py
@@ -1,6 +1,9 @@
 #storing a name in variable "name"
 name = input("Enter your name: ").strip().title()
 
+#Split name first and last
+first, last = name.split()
+
 #printing out whats in variable with a hello message
-print(f"Hello, {name}!")
+print(f"Hello, Mr.{last}!")
 
