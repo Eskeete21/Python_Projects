4839479c6f31e545521f698602427e0474efe4e8
Eddie <eddieskeete@gmail.com>
2026-03-29 02:26:36 -0400

    learning

--- Learning/hello.py
+++ Learning/hello.py
@@ -4,6 +4,9 @@ name = input("Enter your name: ")
 #Remove white spaces
 name = name.strip()
 
+# Capitalize user's name
+name = name.capitalize()
+
 #printing out whats in variable with a hello message
 print(f"Hello, {name}!")
 
