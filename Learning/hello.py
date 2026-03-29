c38092e49ab22933d7cad9d64ccd05202f56cfcb
Eddie <eddieskeete@gmail.com>
2026-03-29 11:00:23 -0400

    learning

--- Learning/hello.py
+++ Learning/hello.py
@@ -1,16 +1,24 @@
+#Keep asking user for gender until a valid gender is input
 while True:
     gender = input("What is your gender (Male or Female): ").title().strip()
     if gender == "Male" or gender == "Female":
         break
     print("Please choose a gender! ")
 
-#storing a name in variable "name"
-name = input("Enter your name: ").strip().title()
+#Keep asking user for first and last name until at least two names are input
+while True:
+    name = input("Enter your first and last name: ").strip().title()
 
-#Split name first and last
-first, last = name.split()
+    #splits name into two parts
+    parts = name.split()
 
-#printing out whats in variable with a hello message
+    #sets variable first and last to parts[0] and parts[1], so first name = parts[0] and last name = parts[1]
+    if len(parts) >= 2:
+        first, last = parts[0], parts[1]
+        break
+    print("Please enter first and last name!")
+
+#printing out last name with hello message 
 
 if gender == "Female":
     print(f"Hello, Ms.{last}!")
