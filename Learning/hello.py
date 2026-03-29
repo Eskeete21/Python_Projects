3fca43a2da32137c70d22cad89edbd38d2e461c1
Eddie <eddieskeete@gmail.com>
2026-03-29 02:24:45 -0400

    learning

--- Learning/hello.py
+++ Learning/hello.py
@@ -1,6 +1,9 @@
 #storing a name in variable "name"
 name = input("Enter your name: ")
 
+#Remove white spaces
+name = name.strip()
+
 #printing out whats in variable with a hello message
 print(f"Hello, {name}!")
 
