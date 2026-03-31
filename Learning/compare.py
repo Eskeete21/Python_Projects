73764183d064f038d93089d9fed918f852e5ad17
Eddie <eddieskeete@gmail.com>
2026-03-31 19:49:29 -0400

    learning

--- Learning/compare.py
+++ Learning/compare.py
@@ -1,5 +1,7 @@
 x = int(input("Enter a number 'x': "))
 y = int(input("Enter a number 'y': "))
 
-if x < y:
-    print(f"x({x}) is less than y({y})")
+if x != y:
+    print("x is not equal to y")
+else:
+    print("x is equal to y")
