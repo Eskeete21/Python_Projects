4d8fb700f6ee6726a976e370c6e0e23ca7867b0a
Eddie <eddieskeete@gmail.com>
2026-04-06 18:17:23 -0400

    learning

--- Learning/cat.py
+++ Learning/cat.py
@@ -1,1 +1,18 @@
-print("meow\n" * 3, end="")
+def main():
+    number = get_number()
+    meow(number)
+
+
+def meow(n):
+    for _ in range(n):
+        print("meow")
+
+
+def get_number():
+    while True:
+        num = int(input("Enter a number: "))
+        if num > 0:
+            return num
+
+
+main()
