9cfb7d509d26c971a15b80e24e802fc508f7451e
Eddie <eddieskeete@gmail.com>
2026-04-08 22:15:00 -0400

    learning

--- Games/snake.py
+++ Games/snake.py
@@ -1,1 +1,16 @@
-print("")
+import tkinter
+import random
+
+ROWS = 25
+COLS = 25
+TITLE_SIZE = 25
+
+WINDOW_WIDTH = TITLE_SIZE * COLS
+WINDOW_HEIGHT = TITLE_SIZE * ROWS
+
+#game window
+window = tkinter.Tk()
+window.title("Snake")
+window.resizable(False, False)
+
+window.mainloop()
